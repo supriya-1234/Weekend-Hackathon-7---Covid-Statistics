cb4e0ceac58bacd3c003191655401b95664b4952
supriya-1234 <66170913+supriya-1234@users.noreply.github.com>
2020-12-11 13:41:01 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -10,6 +10,165 @@ const { connection } = require('./connector')
 
 
 
+app.get('/totalRecovered',(req,res) => {
+    connection.aggregate(([
+        {$group:
+            {_id: null,
+                totalRecovered: { $sum:"$recovered"} 
+            }
+        }  
+            
+    ]))
+    .then((result,rej)=>{
+            
+            let response = {
+                            "data":
+                            {
+                                "_id":"total",
+                                "recovered" : result[0].totalRecovered
+                            }
+                        }
+
+            res.json(response);
+    })
+    .catch((err)=>{
+        res.status(404).send(err);
+    })
+
+})
+
+app.get('/totalActive',(req,res) => {
+
+    connection.aggregate(([
+        {$group:
+            {
+                _id: null,
+                totalActive: {$sum:{$subtract:["$infected","$recovered"]}}}  
+            
+        }
+    ]))
+    .then((result,rej)=>{
+
+            let response = {
+                            "data":
+                            {
+                                "_id":"total",
+                                "active" : result[0].totalActive
+                            }
+                        }
+
+
+            res.json(response);
+    })
+    .catch((err)=>{
+        res.status(404).send(err);
+    })
+
+
+})
+
+app.get('/totalDeath',(req,res) => {
+    connection.aggregate(([
+        {$group:
+            {
+                _id:null,
+                totalDeath: {$sum : "$death"} }  
+            
+        }
+    ]))
+    .then((result,rej)=>{
+
+            // let totalDeath = result.reduce((result,death)=>{
+            //     return result + death.total;
+            // },0);
+            let response = {
+                            "data":
+                            {
+                                "_id":"total",
+                                "death" : result[0].totalDeath
+                            }
+                        }
+
+            res.json(response);
+    })
+    .catch((err)=>{
+        res.status(404).send(err);
+    })
+
+})
+
+app.get('/hotspotStates',(req,res) => {
+
+        connection.aggregate(([
+            {$project: 
+                
+                {
+                    state : 1,
+                    rate: 
+                    { $round : 
+                        [{ $divide: 
+                            [ {$subtract: ["$infected","$recovered"]}, "$infected" ] 
+                        },5]
+                    }
+                }
+                
+            },
+            {
+                $match: { rate: {$gt : 0.1} }
+            }
+        ]))
+        .then((result,rej)=>{
+                console.log(result);
+                let hotspots = result.map((hotspot)=>{
+                    return {"state": hotspot.state,"rate": hotspot.rate}
+                })
+                let response = {
+                    "data": hotspots
+                }
+    
+                res.json(response);
+        })
+        .catch((err)=>{
+            res.status(404).send(err);
+        })
+
+    })
+
+
+app.get('/healthyStates',(req,res) => {
+
+    connection.aggregate(([
+        {$project: 
+            
+                {state : 1,
+                    mortality: 
+                    { $round : 
+                        [
+                            { $divide:[ "$death","$infected" ] },5
+                        ]
+                    }
+                }
+            
+        },{
+            $match: { mortality: {$lt : 0.005} }
+        }
+    ]))
+    .then((result,rej)=>{
+            
+            let healthyStates = result.map((healthyState)=>{
+                return {"state": healthyState.state,"mortality": healthyState.mortality}
+            })
+            let response = {
+                "data": healthyStates
+            }
+
+            res.json(response);
+    })
+    .catch((err)=>{
+        res.status(404).send(err);
+    })
+
+})
 
 
 
